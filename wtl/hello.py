73ca50650ff3afcf3596f9446696c458f8b98ed2
Watal M. Iwasaki <heavy.watal@gmail.com>
2017-03-07 11:52:26 +0900

    Reinforce hello.py

--- wtl/hello.py
+++ wtl/hello.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Sample module
+"""
+
+
 def main():
     import getpass
     print('Hello, ' + getpass.getuser() + '!')
+
+
+if __name__ == '__main__':
+    main()
