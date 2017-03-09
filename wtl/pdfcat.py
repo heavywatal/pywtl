fb2fe386e2d9bcf527066487e69b20e3d12b21da
Watal M. Iwasaki <heavy.watal@gmail.com>
2017-03-09 22:11:01 +0900

    Add postcard mode to pdfcat

--- wtl/pdfcat.py
+++ wtl/pdfcat.py
@@ -3,7 +3,7 @@
 """
 Concatenate PDFs
 """
-from PyPDF2 import PdfFileMerger, PdfFileReader
+from PyPDF2 import PdfFileMerger, PdfFileReader, PdfFileWriter
 
 
 def cat(infiles, outfile):
@@ -16,13 +16,30 @@ def cat(infiles, outfile):
     merger.write(outfile)
 
 
+def merge(labelfile, picturefile, outfile):
+    picpage = PdfFileReader(open(picturefile, 'rb')).getPage(0)
+    reader = PdfFileReader(open(labelfile, 'rb'))
+    writer = PdfFileWriter()
+    for labpage in reader.pages:
+        writer.addPage(labpage)
+        writer.addPage(picpage)
+    print('Writing ' + outfile)
+    with open(outfile, 'wb') as fout:
+        writer.write(fout)
+
+
 def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('-o', '--outfile', default='_merged.pdf')
+    parser.add_argument('-p', '--postcard')
     parser.add_argument('infile', nargs='+')
     args = parser.parse_args()
-    cat(args.infile, args.outfile)
+
+    if args.postcard:
+        merge(args.infile[0], args.postcard, args.outfile)
+    else:
+        cat(args.infile, args.outfile)
 
 
 if __name__ == '__main__':
