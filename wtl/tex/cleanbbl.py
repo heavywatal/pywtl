5667100c3897f19ee618e0cf1776243eb35db640
Watal M. Iwasaki <heavy.watal@gmail.com>
2017-03-13 22:48:25 +0900

    Add tex subpackage

---
+++ wtl/tex/cleanbbl.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+"""
+import re
+
+abbrevs = [
+    'Acad', 'Adv', 'Am', 'Ann', 'Annu', 'Appl',
+    'Biol', 'Biosci', 'Br', 'Bull',
+    'Chem', 'Clin', 'Commun', 'Comp', 'Comput', 'Curr',
+    'Dev', 'Differ', 'Dyn',
+    'Ecol', 'Eng', 'Engl', 'Environ', 'Evol', 'Exp', 'Front',
+    'Genet', 'Hered', 'Int', 'Interdiscip', 'J', 'Lett', 'Lond',
+    'Math', 'Med', 'Mol', 'Microbiol', 'N', 'Nat', 'Natl',
+    'Oncol', 'Opin', 'Philos', 'Phys', 'Popul', 'Proc',
+    'R', 'Relat', 'Rep', 'Res', 'Rev',
+    'Sci', 'Soc', 'Stat', 'Syst', 'Theor', 'Trans', 'Zool']
+
+
+def sub_pagerange(string):
+    def repl(mobj):
+        (start, end) = mobj.groups()
+        if int(start) > int(end):
+            end = start[:-len(end)] + end
+            return '{}--{}'.format(start, end)
+        else:
+            return mobj.group()
+    return re.sub(r'(\d+)--(\d+)', repl, string)
+
+
+def sub_abbrev(string):
+    string = re.sub(r'\bU\s+S\s+A\b', 'USA', string)
+    patt = '(' + '|'.join(abbrevs) + r')(?=\s)'
+    return re.sub(patt, r'\1.', string)
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('bbl', type=argparse.FileType('r+'))
+    args = parser.parse_args()
+    content = args.bbl.read()
+    args.bbl.seek(0)
+    content = sub_pagerange(content)
+    content = sub_abbrev(content)
+    args.bbl.write(content)
+    args.bbl.truncate()
+
+
+if __name__ == '__main__':
+    main()
---
+++ wtl/tex/cleanbib.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Remove meta data of Bibdesk
+"""
+import re
+import sys
+
+
+def rm_comment(string):
+    mobj = re.search('@comment{.+?}\s*}(.+?)@comment{', string, re.DOTALL)
+    return mobj.group(1)
+
+
+def rm_annote(string):
+    '''Be careful when using {braces} or other special characters'''
+    return re.sub(',\s+Annote = {.*?}(?=[,}])', '', string, flags=re.DOTALL)
+
+
+def rename(string):
+    patt = '(Doi|Isbn|Issn|Month)( = )'
+    return re.sub(patt, r'\1-x\2', string, flags=re.DOTALL)
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--outfile',
+                        type=argparse.FileType('w'), default=sys.stdout)
+    parser.add_argument('infile', type=argparse.FileType('r'))
+    args = parser.parse_args()
+    content = args.infile.read()
+    content = rm_comment(content)
+    content = rm_annote(content)
+    content = rename(content)
+    args.outfile.write(content)
+
+
+if __name__ == '__main__':
+    main()
