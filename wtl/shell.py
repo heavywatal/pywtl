2f7e4c26ee87de5f5d71deca918f401e3aca4b74
Watal M. Iwasaki <heavy.watal@gmail.com>
2017-03-14 19:32:12 +0900

    Add dry_run option; forbid rm in args

--- wtl/shell.py
+++ wtl/shell.py
@@ -3,18 +3,26 @@
 """Concurrent shell execution
 """
 import concurrent.futures as confu
-import multiprocessing as mp
 import subprocess
+import re
+from multiprocessing import cpu_count
 
 
-def run(command):
+def run(command, dry_run=False):
+    if not isinstance(command, str):
+        command = ' '.join(command)
+    if re.search(r'\brm\b', command):
+        print(command)
+        raise ValueError('rm is not allowed')
+    if dry_run:
+        command = 'echo ' + command
     return subprocess.run(command, stdout=subprocess.PIPE,
                           stderr=subprocess.STDOUT, shell=True)
 
 
-def map(commands, max_workers=mp.cpu_count()):
+def map(commands, max_workers=cpu_count(), dry_run=False):
     with confu.ThreadPoolExecutor(max_workers=max_workers) as executor:
-        futures = [executor.submit(run, cmd) for cmd in commands]
+        futures = [executor.submit(run, cmd, dry_run) for cmd in commands]
         for future in confu.as_completed(futures):
             completed = future.result()
             print([completed.args, completed.returncode])
@@ -24,10 +32,11 @@ def map(commands, max_workers=mp.cpu_count()):
 def main():
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('-j', '--jobs', type=int, default=mp.cpu_count())
+    parser.add_argument('-n', '--dry-run', action='store_true')
+    parser.add_argument('-j', '--jobs', type=int, default=cpu_count())
     parser.add_argument('command', nargs='+')
     args = parser.parse_args()
-    map(args.command, args.jobs)
+    map(args.command, args.jobs, args.dry_run)
 
 
 if __name__ == '__main__':
