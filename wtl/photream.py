00dd39e5c2bcf52391232e0c5d604a0cf4914a69
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-05-05 18:16:12 +0900

    :art: Import sub and photream from hg/local

---
+++ wtl/photream.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+copy images from Photo Stream to one place
+"""
+import os
+import re
+import shutil
+import datetime
+
+from PIL import Image, ExifTags
+
+APPSUPP = os.path.expanduser('~/Library/Application Support')
+ASSETS = os.path.join(APPSUPP, 'iLifeAssetManagement/assets')
+PHOTOS = os.path.expanduser('~/Pictures/Photos Library.photoslibrary/Masters')
+
+
+def ls_recursive(topdir=PHOTOS):
+    patt = re.compile(r'jpg$|png$', re.IGNORECASE)
+    paths = []
+    for (root, dirs, files) in os.walk(topdir):
+        for afile in files:
+            if patt.search(afile):
+                paths.append(os.path.join(root, afile))
+    return sorted(paths, key=os.path.basename)
+
+
+def decode_exif(image):
+    ret = dict()
+    for (tag, value) in image._getexif().items():
+        ret[ExifTags.TAGS.get(tag)] = value
+    return ret
+
+
+def parse_filename(src):
+    mobj = re.search(r'(pub|sub-shared|watch)/', src)
+    if mobj:
+        label = mobj.group(1)[0]
+    else:
+        label = ''
+    sec = os.path.getmtime(src)
+    dt = datetime.datetime.fromtimestamp(sec)
+    time_stamp = dt.isoformat()
+    if src.lower().endswith('jpg'):
+        img = Image.open(src)
+        exif = decode_exif(img)
+        exif_time = exif.get('DateTimeOriginal')
+        if exif_time:
+            time_stamp = exif_time.replace(' ', 'T')
+    time_stamp = time_stamp.replace(':', '-')
+    basename_ = re.search(r'([a-z]?IMG_\S+)$', src).group(1)
+    return '_'.join([time_stamp, label + basename_])
+
+
+def transfer(src, dst, delete=False, dry_run=False):
+    func = shutil.move if delete else shutil.copy2
+    if os.path.exists(dst):
+        # temporal code
+        if delete and os.path.basename(src) != os.path.basename(dst):
+            print('duplicate: ' + ' '.join([src, dst]))
+            if not dry_run:
+                os.remove(src)
+    else:
+        print([src, dst])
+        if not dry_run:
+            func(src, dst)
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--dry-run', action='store_true')
+    parser.add_argument('-d', '--delete', action='store_true')
+    parser.add_argument('-i', '--iphoto', action='store_true')
+    parser.add_argument('-o', '--outdir', default=os.path.expanduser('~/tmp'))
+    parser.add_argument('files', nargs='*')
+    args = parser.parse_args()
+
+    if args.files:
+        for src in args.files:
+            dst = os.path.join(args.outdir, parse_filename(src))
+            transfer(src, dst, args.delete, args.dry_run)
+    elif args.iphoto:
+        assert(not args.delete)
+        outdir = os.path.expanduser('~/Pictures/PhotoStream')
+        assets = ('pub', 'sub', 'sub-shared', 'watch')
+        for label in assets:
+            top = os.path.join(ASSETS, label)
+            for src in ls_recursive(top):
+                dst = os.path.join(outdir, parse_filename(src))
+                transfer(src, dst, args.delete, args.dry_run)
+    else:
+        assert(not args.delete)
+        outdir = os.path.expanduser('~/Pictures/PhotosBackup')
+        for src in ls_recursive():
+            dst = os.path.join(outdir, parse_filename(src))
+            transfer(src, dst, args.delete, args.dry_run)
+
+
+if __name__ == "__main__":
+    main()
---
+++ wtl/sub.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+"""
+import re
+import shutil
+
+
+def sub(pattern, repl, files, dry_run=False, line_number=False):
+    rex = re.compile(pattern)
+    if repl is None:
+        repl = pattern
+        dry_run = True
+    for filename in files:
+        try:
+            with open(filename, 'r+') as fio:
+                product = []
+                subn_lines = []
+                subn_sum = 0
+                for (i, line) in enumerate(fio.readlines()):
+                    (subn_line, n) = rex.subn(repl, line)
+                    product.append(subn_line)
+                    if n > 0:
+                        subn_sum += n
+                        emphline = rex.sub(emphasize(repl or pattern), line)
+                        if line_number:
+                            ol = ':'.join([filename, str(i + 1), emphline])
+                        else:
+                            ol = ':'.join([filename, emphline])
+                        subn_lines.append(ol)
+                if subn_sum > 0:
+                    if not dry_run:
+                        shutil.copy2(filename, filename + '~')
+                        fio.seek(0)
+                        fio.write(''.join(product))
+                        fio.truncate()
+                    if subn_lines:
+                        print(''.join(subn_lines).rstrip('\n'))
+        except IOError as error:
+            print(error)
+    return
+
+
+def emphasize(s):
+    if hasattr(s, "__call__") and s.__name__ == "camel2snake":
+        def wrap_em(mobj):
+            return emphasize(camel2snake(mobj))
+        s = wrap_em
+    else:
+        s = '\033[1m' + s + '\033[0m'
+    return s
+
+
+def camel2snake(matchobj):
+    s = ''
+    for c in matchobj.group():
+        if c.isupper():
+            s += '_' + c.lower()
+        else:
+            s += c
+    return s
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--line-number", action='store_true')
+    parser.add_argument("-s", "--dry-run", action='store_true')
+    parser.add_argument("-r", "--repl", default=None)
+    parser.add_argument("-w", "--whitespace", action='store_true')
+    parser.add_argument("-c", "--decamel", action='store_const',
+                        dest="repl", const=camel2snake)
+    parser.add_argument("pattern")
+    parser.add_argument("files", nargs='*')
+    args = parser.parse_args()
+    if args.whitespace:
+        args.files.insert(0, args.pattern)
+        args.pattern = " +$"
+        args.repl = ''
+    sub(args.pattern, args.repl, args.files, args.dry_run, args.line_number)
+
+
+if __name__ == '__main__':
+    main()
