669070a974197f1c5e548e09bdd9a9be2dc7889c
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-12-06 22:32:20 +0900

    :sparkles: Create argify.py to parse json

---
+++ wtl/argify.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Convert clippson JSON to command line arguments
+"""
+import json
+
+
+def argify(obj):
+    args = []
+    for key, value in obj.items():
+        if len(key) > 1:
+            args.append(f'--{key}={value}')
+        else:
+            args.append(f'-{key} {value}')
+    return args
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('infile', nargs='?', default='-',
+                        type=argparse.FileType('r'))
+    args = parser.parse_args()
+    obj = json.load(args.infile)
+    result = argify(obj)
+    print(' '.join(result))
+
+
+if __name__ == '__main__':
+    main()
