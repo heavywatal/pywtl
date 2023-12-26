9a4375d3597e683cc902ac5850a210c122e389d3
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2023-12-27 01:13:25 +0900

    :sparkles: Create shorthand of clang-format | diff

--- wtl/clangfmt.py
+++ wtl/clangfmt.py
@@ -16,22 +16,31 @@ presets = ["LLVM", "GNU", "Google", "Chromium", "Microsoft", "Mozilla", "Webkit"
 def main():
     user_config = Path("~/.clang-format").expanduser()
     parser = cli.ArgumentParser()
-    parser.add_argument("-f", "--file", type=Path, default=user_config)
+    parser.add_argument("-c", "--config", action="store_true")
+    parser.add_argument("file", nargs="+", type=Path)
     args = parser.parse_args()
-    with args.file.open("rb") as fin:
+    if args.config:
+        check_config(user_config)
+        return
+    for f in args.file:
+        fmtdiff(f)
+
+
+def check_config(file: Path) -> None:
+    with file.open("rb") as fin:
         obj = yaml.safe_load(fin)
     cnt = count(collect(obj))
     ordered = cnt.most_common()
     print(ordered)
     base_name = obj.get("BasedOnStyle", "LLVM")
     base = clang_format_dump_config(base_name)
-    differed = diff(obj, base)
-    print(yaml.safe_dump(differed))
+    diffs = config_diff(obj, base)
+    print(yaml.safe_dump(diffs))
 
 
-def collect(config: dict[str, Any]):
+def collect(config: dict[str, Any]) -> dict[str, list[str]]:
     configs = {x: clang_format_dump_config(x) for x in presets}
-    res: dict[str, Any] = {}
+    res: dict[str, list[str]] = {}
     for key, value in config.items():
         if key == "BasedOnStyle":
             continue
@@ -60,14 +69,14 @@ def count(collected: dict[str, list[str]]) -> Counter[str]:
     return counter
 
 
-def diff(lhs: dict[str, Any], rhs: dict[str, Any]):
-    differed: dict[str, Any] = {}
+def config_diff(lhs: dict[str, Any], rhs: dict[str, Any]) -> dict[str, str]:
+    diffs: dict[str, str] = {}
     for key, value in lhs.items():
         if key == "BasedOnStyle":
             continue
         if value != rhs.get(key):
-            differed[key] = f"{value}  # {rhs.get(key)}"
-    return differed
+            diffs[key] = f"{value}  # {rhs.get(key)}"
+    return diffs
 
 
 def clang_format_dump_config(style: str) -> dict[str, Any]:
@@ -76,6 +85,16 @@ def clang_format_dump_config(style: str) -> dict[str, Any]:
     with subprocess.Popen(args, stdout=subprocess.PIPE) as p:
         assert p.stdout is not None
         return yaml.safe_load(p.stdout)
+
+
+def fmtdiff(file: Path) -> None:
+    fargs: list[str | Path] = ["clang-format", file]
+    dargs: list[str | Path] = ["diff", "-u", "--color", file, "-"]
+    with (
+        subprocess.Popen(fargs, stdout=subprocess.PIPE) as pfmt,
+        subprocess.Popen(dargs, stdin=pfmt.stdout) as pdiff,
+    ):
+        pdiff.communicate()
 
 
 if __name__ == "__main__":
