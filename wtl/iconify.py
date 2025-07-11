1b74d58491aea8c8d02105c9e9ad27ec0a1bfeaf
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2025-07-11 22:54:14 +0900

    :art: Change toml input to iconify.py

--- wtl/iconify.py
+++ wtl/iconify.py
@@ -1,12 +1,25 @@
-"""Generate CSS via Iconify API for Hugo with Sass."""
+"""Generate CSS via Iconify API for Hugo with Sass.
 
+Icon sets are read from a file or stdin in TOML format like this:
+```
+[[sets]]
+prefix = "bi"
+icons = ["file", "folder"]
+
+[[sets]]
+prefix = "mdi"
+icons = ["home", "user"]
+```
+"""
+
+import argparse
 import logging
 import re
-import subprocess
 import tomllib
 import urllib.parse
 from collections.abc import Iterable
 from pathlib import Path
+from typing import TypedDict
 
 import requests
 
@@ -15,37 +28,41 @@ from wtl import cli
 _log = logging.getLogger(__name__)
 
 
+class IconSet(TypedDict):
+    prefix: str
+    icons: list[str]
+
+
 def main() -> None:
     parser = cli.ArgumentParser()
-    parser.add_argument(
-        "-f", "--force", action="store_true", help="Overwrite existing files."
-    )
-    parser.add_argument(
-        "-i", "--infile", type=Path, help="TOML file. Otherwise read from hugo config."
-    )
     parser.add_argument("-o", "--outdir", type=Path, default=Path())
+    parser.add_argument("infile", nargs="?", type=argparse.FileType("r"), default="-")
     args = parser.parse_args()
-    sets = read_config(args.infile)
-    if not cli.dry_run:
-        args.outdir.mkdir(parents=True, exist_ok=True)
-        make_index_scss(args.outdir, sets.keys())
-    for prefix, icons in sets.items():
+    toml = args.infile.read()
+    args.infile.close()
+    sets: list[IconSet] = tomllib.loads(toml)["sets"]
+    _log.info(f"{sets = }")
+    make_index_scss(args.outdir, [x["prefix"] for x in sets])
+    for item in sets:
+        prefix = item["prefix"]
         outfile = args.outdir / f"{prefix}.css"
-        if outfile.exists() and not args.force:
-            _log.info(f"{outfile} already exists.")
-            continue
-        css = fetch_css(prefix, icons)
+        css = fetch_css(prefix, item["icons"])
+        _log.debug(f"{css}")
         css = postprocess(css)
         if not cli.dry_run:
             with outfile.open("w") as fout:
                 fout.write(css)
+        print(outfile)
 
 
 def make_index_scss(outdir: Path, names: Iterable[str]) -> None:
-    index_scss = outdir / "_index.scss"
-    with index_scss.open("w") as fout:
-        for name in names:
-            fout.write(f'@forward "{name}";\n')
+    outfile = outdir / "_index.scss"
+    if not cli.dry_run:
+        outdir.mkdir(parents=True, exist_ok=True)
+        with outfile.open("w") as fout:
+            for prefix in names:
+                fout.write(f'@forward "{prefix}";\n')
+    print(outfile)
 
 
 def fetch_css(prefix: str, icons: list[str]) -> str:
@@ -56,9 +73,7 @@ def fetch_css(prefix: str, icons: list[str]) -> str:
         "iconSelector": "iconify-icon[icon=@{prefix}:{name}@]",
     }
     if cli.dry_run:
-        unparsed = url + "?" + urllib.parse.urlencode(params)
-        print(unparsed)
-        return unparsed
+        return url + "?" + urllib.parse.urlencode(params)
     response = requests.get(url, params=params, timeout=5)
     return response.text
 
@@ -68,27 +83,6 @@ def postprocess(css: str) -> str:
     css = re.sub(r"commonSelector[^}]+}\n*", "", css)
     css = re.sub(r"\n+", "\n", css)
     return css  # noqa: RET504
-
-
-def read_config(infile: Path | None) -> dict[str, list[str]]:
-    """Read icon sets from Hugo configuration in TOML format.
-
-    Icons should be listed by prefix in the `params.iconify` section, e.g.:
-    ```
-    [params.iconify]
-    bi = ["file", "folder"]
-    mdi = ["home", "user"]
-    ```
-    """
-    if infile is None:
-        cmd = ["hugo", "config"]
-        p = subprocess.run(cmd, stdout=subprocess.PIPE, check=True)
-        toml = p.stdout.decode()
-    else:
-        with infile.open("rt") as fin:
-            toml = fin.read()
-    data = tomllib.loads(toml)
-    return data.get("params", {}).get("iconify", {})
 
 
 if __name__ == "__main__":
