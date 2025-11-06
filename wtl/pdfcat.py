2e231b2d36ac425543f1a3e1d1a4eff2e93d6c2e
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2025-11-07 00:12:34 +0900

    feat: Replace gs with qpdf

--- wtl/pdfcat.py
+++ wtl/pdfcat.py
@@ -2,29 +2,24 @@
 
 import subprocess
 from collections.abc import Iterable
+from pathlib import Path
 
 from . import cli
 
 
-def gs_pdfwrite(
-    infiles: Iterable[str], outputfile: str, *, quiet: bool = False
-) -> None:
-    args = ["gs", "-dNOPAUSE", "-dBATCH", "-sDEVICE=pdfwrite"]
-    if quiet:
-        args.append("-q")
-    args.append(f"-sOutputFile={outputfile}")
-    args.extend(infiles)
-    print(" ".join(args))
-    if not cli.dry_run:
-        subprocess.run(args, check=True)
-
-
 def main() -> None:
     parser = cli.ArgumentParser()
-    parser.add_argument("-o", "--outfile", default="_merged.pdf")
-    parser.add_argument("infiles", nargs="+")
+    parser.add_argument("-o", "--outfile", type=Path, default="_concatenated.pdf")
+    parser.add_argument("infiles", nargs="+", type=Path)
     args = parser.parse_args()
-    gs_pdfwrite(args.infiles, args.outfile, quiet=args.verbosity <= 0)
+    qpdf_empty_pages(args.infiles, args.outfile)
+
+
+def qpdf_empty_pages(infiles: Iterable[Path], outfile: Path) -> None:
+    args = ["qpdf", "--empty", "--pages", *infiles, "--", outfile]
+    print(" ".join(str(x) for x in args))
+    if not cli.dry_run:
+        subprocess.run(args, check=True)
 
 
 if __name__ == "__main__":
